04a5da4292d19a3beddefe15923a969428489274
Matthias Bussonnier <bussonniermatthias@gmail.com>
2022-08-04 16:53:59 +0200

    impute

--- setup.py
+++ setup.py
@@ -3,6 +3,15 @@ import re
 import setuptools
 
 
+from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+
+
+class BdistWheel(_bdist_wheel):
+    def finalize_options(self):
+        _bdist_wheel.finalize_options(self)
+        self.root_is_pure = False
+
+
 setuptools.setup(
     name="tree_sitter_rst",
     version="0.0.1",
@@ -15,4 +24,5 @@ setuptools.setup(
     packages=["tree_sitter_rst"],
     package_data={"tree_sitter_rst": ["rst.so"]},
     install_requires=["tree-sitter"],
+    cmdclass={"bdist_wheel": BdistWheel},
 )
